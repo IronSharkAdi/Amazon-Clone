0f869439034c169937d2a5f283c93e0c0dd36b23
Adi <aditto2001@gmail.com>
2020-12-07 01:04:00 +0600

    adi2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,6 +8,18 @@ function login() {
             <Link>
             <img className="login__logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1920px-Amazon_logo.svg.png" alt="amazon logo png"/>
             </Link>
+            <div className="login__container">
+                <h1>Sign-in</h1>
+                <form>
+                    <h5>Email</h5>
+                    <input type="text"/>
+                    <h5>Password</h5>
+                    <input type="password"/>
+                    <button className="sign-in__btn">Sign in</button>
+                </form>
+                <p>By signing in you agree to this Amazon clone conditions of use & sell . Actually there is no conditions or something , this is just a non-commercial project</p>
+                <button className="login__btn">Create you account</button>
+            </div>
         </div> 
     )
 }
